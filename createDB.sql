6a088d9e28d2249491fa7f77014c513d61f9bd56
Stefan Busemann <stefan.busemann@in2code.de>
2023-07-25 11:08:10 +0200

    [TASK] Add example files

---
+++ createDB.sql
@@ -0,0 +1,11 @@
+#
+# Table structure for table 'fe_users'
+#
+CREATE TABLE fe_users
+(
+    yourName      varchar(60) DEFAULT '',
+    password      varchar(50) DEFAULT '',
+    date_of_birth int(11) DEFAULT '0' NOT NULL,
+    privacy       tinyint(4) unsigned DEFAULT '0' NOT NULL,
+    comments      text NOT NULL
+);
---
+++ registerUser.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    } else {
+        $name = test_input($_POST["name"]);
+        // check if name only contains letters and whitespace
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+            $nameErr = "Only letters and white space allowed";
+        }
+    }
+
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+        // check if e-mail address is well-formed
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "Invalid email format";
+        }
+    }
+
+    if (empty($_POST["website"])) {
+        $website = "";
+    } else {
+        $website = test_input($_POST["website"]);
+        // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+        if (!preg_match(
+            "/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",
+            $website
+        )) {
+            $websiteErr = "Invalid URL";
+        }
+    }
+
+    if (empty($_POST["comment"])) {
+        $comment = "";
+    } else {
+        $comment = test_input($_POST["comment"]);
+    }
+
+    if (empty($_POST["gender"])) {
+        $genderErr = "Gender is required";
+    } else {
+        $gender = test_input($_POST["gender"]);
+    }
+}
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+?>
+
+
+<div class="container">
+    <h2>PHP Form Validation Example</h2>
+
+
+    <?php
+    echo "<h2>Your Input:</h2>";
+    echo var_dump($_POST);
+    echo "<br>";
+    ?>
+
+</div>
+
+</html>
